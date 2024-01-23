0698f533eb0188d6b7423dc8ddc53da281698ce8
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-23 14:02:31 +0100

    00 / kneel you fool

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,12 @@ package com.cc.java;
 public class App 
 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World! Kneel for they highness, the great cat!");
+        System.out.println("Praise the cat! Experience the scent of catnip!");
+        System.out.println("I'm afraid i can't do that, Dave.");
+        
     }
     
 }
